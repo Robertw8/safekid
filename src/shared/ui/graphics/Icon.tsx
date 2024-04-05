f0ce04fa55ce68b3f75550ac659362a452ec5332
Robertw8 <oniedgaf@gmail.com>
2024-04-05 21:14:07 +0200

    add Icon component and icons for welcome screen

---
+++ src/shared/ui/graphics/Icon.tsx
@@ -0,0 +1,15 @@
+import { styled } from 'nativewind';
+import { SvgXml } from 'react-native-svg';
+
+interface IconProps {
+  xml: string;
+  classNames?: string;
+}
+
+const StyledSvg = styled(SvgXml);
+
+const Icon: React.FC<IconProps> = ({ xml, classNames }) => {
+  return <StyledSvg xml={xml} className={` ${classNames}`} />;
+};
+
+export default Icon;
--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -1,1 +1,6 @@
-export { PrimaryButton } from './buttons';
+export { default as PrimaryButton } from './buttons/PrimaryButton';
+export { default as PrimaryTitle } from './typography/PrimaryTitle';
+export { default as SecondaryTitle } from './typography/SecondaryTitle';
+export { default as Icon } from './graphics/Icon';
+export { default as adultIcon } from './graphics/icons/adult-icon';
+export { default as kidIcon } from './graphics/icons/kid-icon';
