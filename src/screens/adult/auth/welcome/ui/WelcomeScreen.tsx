99133cd84472fe10e6d8ad7ff7fbadaa918d31cb
Robertw8 <oniedgaf@gmail.com>
2024-04-15 23:41:05 +0200

    restructure screens folder

--- src/screens/adult/auth/welcome/ui/WelcomeScreen.tsx
+++ src/screens/adult/auth/welcome/ui/WelcomeScreen.tsx
@@ -1,35 +1,39 @@
 import { View } from 'react-native';
 import { Link, router } from 'expo-router';
 import { ClickableRoleCard } from '@/widgets/auth';
-import { SecondaryTitle, adultIcon, kidIcon } from '@/shared/ui';
+import {
+  PrimaryContainer,
+  SecondaryTitle,
+  adultIcon,
+  kidIcon,
+} from '@/shared/ui';
 import { styled } from 'nativewind';
 
-const Wrapper = styled(View);
 const CardsWrapper = styled(View);
 
 const WelcomeScreen: React.FC = () => {
   return (
-    <Wrapper className="px-4 h-full flex justify-center items-center">
-      <SecondaryTitle text="Виберіть свою роль" />
+    <PrimaryContainer>
+      <SecondaryTitle>Виберіть свою роль</SecondaryTitle>
       <CardsWrapper className="mt-8">
         <ClickableRoleCard
           iconXml={adultIcon}
           title="Дорослий"
           description="Lorem ipsum dolor sit amet."
-          onPress={() => router.replace('/register')}
+          onPress={() => router.replace('/adult/register')}
         />
         <ClickableRoleCard
           iconXml={kidIcon}
           title="Дитина"
           description="Lorem ipsum dolor sit amet."
           classNames="mt-4"
-          onPress={() => router.replace('/login')}
+          onPress={() => router.replace('/kid/scan')}
         />
       </CardsWrapper>
       <Link href="/dashboard" style={{ marginTop: 20, color: 'green' }}>
         Dashboard
       </Link>
-    </Wrapper>
+    </PrimaryContainer>
   );
 };
 
