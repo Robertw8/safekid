4b40a9a1799f399a8eb9a977d409e009d149a909
Robertw8 <oniedgaf@gmail.com>
2024-04-19 19:24:57 +0200

    fix re-export cycle

--- src/shared/ui/containers/CheckField.tsx
+++ src/shared/ui/containers/CheckField.tsx
@@ -1,23 +1,30 @@
-import { CheckButton } from '@/shared/ui';
+import CheckButton from '../buttons/CheckButton';
 import { styled } from 'nativewind';
 import { View } from 'react-native';
 
 interface CheckFieldProps {
-    children: React.ReactNode | string;
-    checked: boolean;
-    onPress: () => void;
-    classNames?: string;
-};
+  children: React.ReactNode | string;
+  checked: boolean;
+  onPress: () => void;
+  classNames?: string;
+}
 
 const WrapperCheckField = styled(View);
 
-const CheckField: React.FC<CheckFieldProps> = ({children, checked, onPress, classNames}) => {
-    return (
-        <WrapperCheckField className={`flex flex-row gap-2 items-center mb-6 ${classNames}`}>
-            <CheckButton checked={checked} onPress={onPress} />
-            {children}
-        </WrapperCheckField>
-    )
+const CheckField: React.FC<CheckFieldProps> = ({
+  children,
+  checked,
+  onPress,
+  classNames,
+}) => {
+  return (
+    <WrapperCheckField
+      className={`flex flex-row gap-2 items-center mb-6 ${classNames}`}
+    >
+      <CheckButton checked={checked} onPress={onPress} />
+      {children}
+    </WrapperCheckField>
+  );
 };
 
 export default CheckField;
