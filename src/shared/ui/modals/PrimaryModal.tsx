9be6fa6c11b2746491a785e11f88e81ddb0ef450
Sergio <137483700+KaratSergio@users.noreply.github.com>
2024-04-24 19:44:01 +0300

    Update PrimaryModal.tsx

--- src/shared/ui/modals/PrimaryModal.tsx
+++ src/shared/ui/modals/PrimaryModal.tsx
@@ -1,23 +1,23 @@
 import { styled } from 'nativewind';
 import { Modal, View } from 'react-native';
 
+import { SecondaryTitle, NormalText } from '@/shared/ui';
+
 interface ModalProps {
   visible: boolean;
-  onCancel: () => void;
-  onConfirm: () => void;
-  header?: React.ReactNode;
-  body?: React.ReactNode;
-  footer?: React.ReactNode;
   classNames?: string;
+  body?: React.ReactNode;
+  header?: React.ReactNode;
+  footer?: React.ReactNode;
 }
 
 const ModalContainer = styled(View);
 const ModalContent = styled(View);
+const HeaderTitle = styled(SecondaryTitle);
+const BodyText = styled(NormalText);
 
 const PrimaryModal: React.FC<ModalProps> = ({
   visible,
-  onCancel,
-  onConfirm,
   header,
   body,
   footer,
@@ -26,11 +26,11 @@ const PrimaryModal: React.FC<ModalProps> = ({
   return (
     <Modal visible={visible} transparent animationType="slide">
       <ModalContainer
-        className={` rounded-lg ml-16 mt-20 bg-white w-64 h-64 ${classNames}`}
+        className={` rounded-lg mt-48 bg-slate-50 w-full p-4 ${classNames}`}
       >
         <ModalContent className={`items-center mt-4 ${classNames}`}>
-          {header && <View>{header}</View>}
-          {body && <View>{body}</View>}
+          {header && <HeaderTitle>{header}</HeaderTitle>}
+          {body && <BodyText>{body}</BodyText>}
           {footer && <View>{footer}</View>}
         </ModalContent>
       </ModalContainer>
