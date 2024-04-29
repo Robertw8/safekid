93bbca24e3cf6af305e78b83031a787624e16c30
OlenaIa <121794911+OlenaIa@users.noreply.github.com>
2024-04-29 17:29:06 +0300

    add bearer token

--- src/processes/auth/api/delParentAccount.ts
+++ src/processes/auth/api/delParentAccount.ts
@@ -1,9 +1,11 @@
 import instance from './baseUrl';
+import { token } from './postLoginUser';
 
 const delParentAccount = async (_, thunkApi) => {
     try {
         console.log('use delParentAccount', );
         const response = await instance.delete('parent/delete');
+        token.clear();
         return response.data;
     } catch (error) {
         alert(
