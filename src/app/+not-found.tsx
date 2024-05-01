ddb3a1fe9023b6ade9fad82acf778001d746ba50
Robertw8 <oniedgaf@gmail.com>
2024-05-01 22:36:13 +0200

    add 404 page

--- src/app/+not-found.tsx
+++ src/app/+not-found.tsx
@@ -1,10 +1,23 @@
-import { Text, View } from 'react-native';
+import {
+  Icon,
+  PrimaryButton,
+  PrimaryContainer,
+  SecondaryTitle,
+  errorIcon,
+} from '@/shared/ui';
+import { router } from 'expo-router';
 
 const NotFound: React.FC = () => {
   return (
-    <View>
-      <Text>Not found</Text>
-    </View>
+    <PrimaryContainer classNames="items-center">
+      <Icon xml={errorIcon} />
+      <SecondaryTitle>Сторінку не знайдено :(</SecondaryTitle>
+      <PrimaryButton
+        text="Повернутись назад"
+        classNames="mt-6"
+        onPress={() => router.back()}
+      />
+    </PrimaryContainer>
   );
 };
 
