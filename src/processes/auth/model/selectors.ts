e4e153d01b69a46bdca7bebb7b6b1b648a1fa18b
Aleksandr2905 <natalukha@ukr.net>
2024-05-09 17:31:50 +0300

    fix user info

--- src/processes/auth/model/selectors.ts
+++ src/processes/auth/model/selectors.ts
@@ -2,10 +2,10 @@ import { RootState } from '@/app/providers/StoreProvider';
 
 export const selectRole = (state: RootState) => state.auth.role;
 export const selectUserData = (state: RootState) => state.auth.userData;
-export const selectAuthenticated = (state: RootState) => state.auth.authenticated;
+export const selectAuthenticated = (state: RootState) =>
+  state.auth.authenticated;
 export const selectVerifyEmail = (state: RootState) => state.auth.verifyEmail;
 export const selectUserId = (state: RootState) => state.auth.userId;
+export const selectEmail = (state: RootState) => state.auth.email;
 export const selectToken = (state: RootState) => state.auth.token;
 export const selectIsLoading = (state: RootState) => state.auth.isLoading;
-
-
